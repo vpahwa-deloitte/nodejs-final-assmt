23dd52069661b16f5d62fa386b50b5afd9c0ab44
Vanshaj Pahwa <vpahwa@deloitte.com>
2023-07-22 12:09:18 +0530

    refactor: Update database configuration to connect to the PostgreSQL database hosted on Render.

--- config/database.js
+++ config/database.js
@@ -1,16 +1,31 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize('hashathon', 'postgres', 'jain1234', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+/* // local database
+const sequelize = new Sequelize("hashathon", "postgres", "jain1234", {
+  host: "localhost",
+  dialect: "postgres",
+}); */
+
+// render database
+const sequelize = new Sequelize(
+  "hashathon_fhhm",
+  "vpahwa",
+  "kyqfW1YZJp9qQF8vOLPyCLyKsoiBpglw",
+  {
+    host: "dpg-citn885iuiedpv4qssc0-a.oregon-postgres.render.com",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true,
+    },
+  }
+);
 
 (async () => {
   try {
     await sequelize.authenticate();
-    console.log('Database connection established successfully.');
+    console.log("Database connection established successfully.");
   } catch (error) {
-    console.error('Unable to connect to the database:', error);
+    console.error("Unable to connect to the database:", error);
   }
 })();
 
