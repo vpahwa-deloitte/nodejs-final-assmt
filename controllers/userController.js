1cf4afb204270c1032c4345f5262064c70966fac
Vanshaj Pahwa <vpahwa@deloitte.com>
2023-07-24 16:45:21 +0530

    refactor: Removed comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,10 +12,8 @@ const signUp = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Hash the password before storing it in the database
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create a new user
     const newUser = await User.create({
       full_name,
       email,
@@ -25,10 +23,8 @@ const signUp = async (req, res) => {
       business_unit,
     });
 
-    // Generate a JWT token for the new user
     const token = generateToken({ id: newUser.id });
 
-    // Return the token and user data as the response
     res.json({ message: "User registered successfully", token, user: newUser });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
@@ -45,16 +41,13 @@ const signIn = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Compare the entered password with the hashed password in the database
     const passwordMatch = await bcrypt.compare(password, user.password);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    // Generate a JWT token for the authenticated user
     const token = generateToken({ id: user.id });
 
-    // Return the token and user data as the response
     res.json({ message: "User loggedin successful", token, user });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
